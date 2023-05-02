73b2dfcfbabc32ae8e527b7b7e39277b5ba7b8dd
Raja <xrosama@rakbank.ae>
2023-05-03 00:22:12 +0400

    assets and config

---
+++ src/config/color.tsx
@@ -0,0 +1,16 @@
+export const light = {
+  text: '#393D3F',
+  bg: '#393D3F',
+  header: '#c6c5b9',
+  heading: '#62929e',
+  secondary: '#546a7b',
+};
+
+export const dark = {
+  text: '#070707',
+  bg: '#28231c',
+  header: '#513b3c',
+  heading: '#655356',
+  secondary: '#c1eeff',
+};
+
